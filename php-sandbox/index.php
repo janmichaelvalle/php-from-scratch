df16adc78d9bf38f2dabdafc4247dccce9a7e729
janmichaelvalle <janmichaelvalle@gmail.com>
2024-05-16 10:11:30 +0800

    Finish session and cookie

--- php-sandbox/index.php
+++ php-sandbox/index.php
@@ -1,57 +1,14 @@
-<?php
+<?php 
 
-abstract class Shape {
-    protected $name;
+session_start();
 
-    // Abstract method
-    abstract public function calculateArea();
+$_SESSION['name'] = 'John';
 
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
 
-    // Concrete method
-    public function getName() {
-        return $this->name;
-    }
+if(isset($_SESSION['name'])) {
+    echo 'Name: ' . $_SESSION['name'];
+} else {
+    echo 'Name is not set';
 }
 
-class Circle extends Shape {
-    private $radius;
-
-    public function __construct($name, $radius)
-    {
-        parent::__construct($name);
-        $this->radius = $radius;
-    }
-
-    public function calculateArea()
-    {
-        return pi() * pow($this->radius, 2);
-    }
-}
-
-class Rectangle extends Shape {
-    private $height;
-    private $width;
-
-    public function __construct($name, $width, $height)
-    {
-        parent::__construct($name);
-        $this->width = $width;
-        $this->height = $height;
-    }
-
-    public function calculateArea()
-    {
-        return $this->width * $this->height;
-    }
-}
-
-$circle = new Circle('Circle1', 5);
-var_dump($circle);
-$rectangle = new Rectangle('Rect1', 10, 5);
-var_dump($rectangle);
-
-echo $circle->getName() . 'Area: ' . $circle->calculateArea() . '<br>';
+print_r($_SESSION);

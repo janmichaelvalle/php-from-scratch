dab22eca792a830148e860f6dc9563de10041df1
Mikee <100498679+janmichaelvalle@users.noreply.github.com>
2024-05-24 17:02:52 +0800

    Done with Create Views

--- workopia/helpers.php
+++ workopia/helpers.php
@@ -49,3 +49,28 @@ function loadView($name) {
   }
 }
 
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * return void
+ */
+
+ function inspect($value) {
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+ }
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * return void
+ */
+
+ function inspectAndDie($value) {
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+ }

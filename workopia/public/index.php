0256b7d1b3fed8aae71bef8e207ebf8a70d56e67
janmichaelvalle <janmichaelvalle@gmail.com>
2024-05-27 10:37:45 +0800

    Finish router refactor to class

--- workopia/public/index.php
+++ workopia/public/index.php
@@ -1,8 +1,20 @@
 <?php
 
 require '../helpers.php';
+require basePath('Router.php');
 
+$router = new Router();
 
-$uri = $_SERVER['REQUEST_URI'];
+// Register routes
+require basePath('routes.php');
 
-require basePath('router.php');
+// Get the request URI and method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Debugging: Output the URI and method
+echo "Requested URI: " . $uri . "<br>";
+echo "Requested Method: " . $method . "<br>";
+
+// Route the request
+$router->route($uri, $method);

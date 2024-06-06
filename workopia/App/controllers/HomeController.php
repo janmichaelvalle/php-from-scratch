b8384063b634b348ad232ead4b4943e82f44b9c7
janmichaelvalle <janmichaelvalle@gmail.com>
2024-06-06 10:27:34 +0800

    Done with ErrorController Class

--- workopia/App/controllers/HomeController.php
+++ workopia/App/controllers/HomeController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controllers;
 
+use Framework\Database;
+
 class HomeController {
+  protected $db;
   public function __construct()
   {
-    
+    $config = require basePath('config/db.php');
+    $this->db = new Database($config);
   }
   public function index()
   {
-    die('HomeController@index');
+    $listings = $this->db->query('SELECT * FROM workopia.listings LIMIT 6')->fetchAll();
+
+    loadView('home', [
+      'listings' => $listings      
+    ]);
   }
 }
